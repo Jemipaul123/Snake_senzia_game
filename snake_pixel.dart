7aa50fd574cee8ca3e5628d3eb57b696e3812879
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2023-03-07 21:51:32 +0530

    Create snake_pixel.dart

---
+++ snake_pixel.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SnakePixel extends StatelessWidget{
+  const SnakePixel({Key? key}) :super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(7),
+
+        ),
+      ),
+    );
+  }
+
+}
