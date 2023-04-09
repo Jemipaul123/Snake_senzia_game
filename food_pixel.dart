1e4c58e66f739dbf80d2b1bfcfa4cbcd0961095b
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2023-04-09 14:29:41 +0530

    Update food_pixel.dart

--- food_pixel.dart
+++ food_pixel.dart
@@ -4,6 +4,7 @@ class FoodPixel extends StatelessWidget{
   const FoodPixel({Key? key}) :super(key:key);
 
   @override
+  
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(5.0),
