93965b02feb0a169b2b89890dcdeb5dbebb04302
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2023-04-14 23:54:53 +0530

    Update blank_pixel.dart

--- blank_pixel.dart
+++ blank_pixel.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class BlankPixel extends StatelessWidget{
   const BlankPixel({Key? key}) :super(key:key);
 
+  
   @override
   Widget build(BuildContext context) {
     return Padding(
