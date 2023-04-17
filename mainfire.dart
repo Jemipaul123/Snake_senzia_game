d31062d14c8546c9d8689e78251d5930adbc10cb
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2023-04-17 22:02:28 +0530

    Update mainfire.dart

--- mainfire.dart
+++ mainfire.dart
@@ -7,6 +7,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async{
   
+  
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
