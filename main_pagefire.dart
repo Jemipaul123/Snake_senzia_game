f081d377e75d242f16f36f0acb75e6a43dc348d2
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2023-04-18 23:50:28 +0530

    Update main_pagefire.dart

--- main_pagefire.dart
+++ main_pagefire.dart
@@ -1,7 +1,6 @@
 import 'package:chat_app/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'homepage.dart';
 
 class MainPage extends StatefulWidget {
